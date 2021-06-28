d3add5bda6ea0f5b6adb2461e59b1eaf121356ad
Thiago <sabotamail@gmail.com>
2021-06-28 13:11:22 -0300

    adicinando pasta de exercicios do bloco 4.

---
+++ exercises/bloco_4/dia-3/xadrez.js
@@ -0,0 +1,31 @@
+// Posicao da rainha
+
+// Posicao da peça oponente
+
+// Pode atacar?
+
+// Calculo para entender se o ataque foi bem sucedido ou não
+
+// Se a rainha esta na mesma linha que a peca oponente Ou SE A RAINHA ESTA NA MESMA COLUNA QUE A PECA OPONENTE SIGNIFICAQUE O ATAQUE FOI BEM SUCEDIDO
+
+
+// IDENTIFICAR SE A PEÇA DO OPONENTE ESTÁ  NA DIAGONAL INFERIOR ESQUERDA 
+DECREMENTAR a esquerda 
+
+// IDENTIFICAR SE A PEÇA DO OPONENTE ESTÁ  NA DIAGONAL SUPERIOR ESQUERDA
+
+// // IDENTIFICAR SE A PEÇA DO OPONENTE ESTÁ  NA DIAGONAL INFERIOR DIREITA
+
+// IDENTIFICAR SE A PEÇA DO OPONENTE ESTÁ  NA DIAGONAL INFERIOR ESQUERDA
+//exercise.js
+let fruits = [3, 4, 10, 1, 12];
+let sum = 0;
+for (let index = 0; index < fruits.length; index += 1) {
+  sum += fruits[index];
+  console.log(sum);
+}
+if (sum > 15) {
+    console.log(sum);
+  } else {
+    console.log('valor menor que 16')
+  }
